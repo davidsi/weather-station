a9969a1001cb8fe4afdc1a4a96cb96859c511343
david <david@davids-MacBook-Pro.local>
2017-05-11 20:52:53 -0700

    inint

---
+++ configure.sh
@@ -0,0 +1,12 @@
+sudo npm install -g sleep
+sudo npm install -g querystring
+sudo npm install -g fs
+sudo npm install -g url
+sudo npm install -g formidable
+
+npm link sleep
+npm link querystring
+npm link fs
+npm link url
+npm link formidable
+
